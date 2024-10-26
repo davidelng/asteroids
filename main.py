fef52b4494a0b0540c2840dd98616bb1148b8141
davidelng <davidelngyt@gmail.com>
2024-10-26 19:36:06 +0200

    moving around

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -21,7 +25,12 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60 FPS
